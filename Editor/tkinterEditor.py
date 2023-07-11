c6ec153d39f2c33319a52ba57bc94068025ef999
arischann <liber1238@gmail.com>
2023-07-11 12:55:29 +0900

    격자

--- Editor/tkinterEditor.py
+++ Editor/tkinterEditor.py
@@ -2,40 +2,52 @@ import tkinter
 
 window=tkinter.Tk()
 
+
+
 window.title("tkinter 테스트") #창의 이름
 window.geometry("1200x800+100+100") #창의 너비, 높이
 window.resizable(True, True) #창 크기 조절 가능 여부
 #window.attributes("-fullscreen", True) #전체화면
 
+inputFrame = tkinter.Frame(window, relief="solid", bd=2)
+inputFrame.place(x = 0, y = 0)
+gridFrame = tkinter.Frame(window, relief="solid", bd=2)
+gridFrame.place(x = 400, y = 0)
+
 mapX = 0
 mapY = 0
+gridSize = 8 #격자의 크기
 mapArray = []
 
-def integercheck(): #입력값으로 2차원 배열 크기 변경
+def sizeChange(): #입력값으로 2차원 배열 크기 변경
     if entryX.get().isdigit() and entryY.get().isdigit(): 
         label.config(text = entryX.get() + ", " + entryY.get())
         global mapX
         global mapY
         global mapArray
+        for i in gridFrame.grid_slaves():
+            i.destroy()
         mapX = int(entryX.get())
         mapY = int(entryY.get())
         mapArray = [[0 for y in range(mapY)] for x in range(mapX)]
-        asd()
+        drawgrid()
     else:
         label.config(text = "정수를 입력해주세요...!")
 
-def asd(): #격자 그리기
-
+def drawgrid(): #격자 그리기
+    global gridSize
     for x in range(mapX):
         for y in range(mapY):
-            mapArray[x][y] = tkinter.Label(window, text = "", width = 2, height = 1, relief = "solid", borderwidth = 2).grid(column=x+4 , row = y+4)
+            mapArray[x][y] = tkinter.Label(gridFrame, text = "", width = gridSize*2, height = gridSize, relief = "solid", borderwidth = 2, bg="blue").grid(column=x+4 , row = y+4)
+    print(mapArray)
           
 
+label = tkinter.Label(inputFrame, text = "곧 바뀔 텍스트")
+entryX = tkinter.Entry(inputFrame)
+entryY = tkinter.Entry(inputFrame)
+button = tkinter.Button(inputFrame, text = "확인", command = sizeChange)
 
-label = tkinter.Label(window, text = "곧 바뀔 텍스트")
-entryX = tkinter.Entry(window)
-entryY = tkinter.Entry(window)
-button = tkinter.Button(window, text = "확인", command = integercheck)
+
 
 label.grid()
 entryX.grid()
