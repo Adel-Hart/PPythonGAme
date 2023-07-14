f43e9f4dfb22594d9b9cae61fc9acf6da60a0d44
arischann <liber1238@gmail.com>
2023-07-14 21:37:15 +0900

    .dat 파일 저장 기능

--- Editor/tkinterEditor.py
+++ Editor/tkinterEditor.py
@@ -4,37 +4,37 @@ import tkinter
 import pyautogui
 
 window = tkinter.Tk()
-window.title("tkinter 테스트") #창의 이름
+window.title("맵에디터") #창의 이름
 window.geometry("1200x800+100+100") #창의 너비, 높이
 window.resizable(False, False) #창 크기 조절 가능 여부
 window.attributes("-fullscreen", True) #전체화면
 
-inputFrame = tkinter.Frame(window, relief="solid", bd=2)
+inputFrame = tkinter.Frame(window)
 inputFrame.place(x = 0, y = 0)
 gridFrame = tkinter.Frame(window, relief="solid", bd=2)
 gridFrame.place(x = 600, y = 0)
-
-colorTuple = ("black", "red", "green", "blue", "yellow", "cyan", "magenta", "white")
+brushColor = 3
+colorTuple = ("black", "red", "green", "blue", "yellow", "cyan", "magenta", "white", "gray")
 mapX = 0
 mapY = 0
 gridSize = 50 #격자 한칸의 크기
 mapArray = []
 
-def sizeChange(): #2차원 배열 크기 변경
+def sizeChange(): #2차원 배열 크기 변경, 캔버스 생성
     global mapX
     global mapY
     global mapArray
     global canvas
-    if entryX.get().isdigit() and entryY.get().isdigit(): 
-        label.config(text = entryX.get() + ", " + entryY.get())
+    if XEntry.get().isdigit() and YEntry.get().isdigit(): 
+        mapSizeAlert.config(text = XEntry.get() + ", " + YEntry.get())
         canvas.destroy()
-        mapX = int(entryX.get())
-        mapY = int(entryY.get())
+        mapX = int(XEntry.get())
+        mapY = int(YEntry.get())
         canvas = tkinter.Canvas(gridFrame, width = mapX * gridSize, height = mapY * gridSize)
         mapArray = [[0 for y in range(mapY)] for x in range(mapX)]
         drawGrid()
     else:
-        label.config(text = "정수를 입력해주세요...!")
+        mapSizeAlert.config(text = "정수를 입력해주세요...!")
 
 def drawGrid(): #격자 그리기
     global canvas
@@ -50,27 +50,129 @@ def colorChange(event): #색 변경
     global mapArray
     x = (pyautogui.position()[0]-600) // gridSize 
     y = (pyautogui.position()[1]) // gridSize
-    mapArray[x][y] = 1
-    canvas.create_rectangle(x * gridSize, y * gridSize, x * gridSize + gridSize, y * gridSize + gridSize, fill = colorTuple[1])
+    if x < mapX and y < mapY:
+        mapArray[x][y] = brushColor
+        canvas.create_rectangle(x * gridSize, y * gridSize, x * gridSize + gridSize, y * gridSize + gridSize, fill = colorTuple[brushColor])
 
-def debug():
-    print(mapArray)
+def blackBrush():
+    global brushColor
+    brushColor = 0
+def redBrush():
+    global brushColor
+    brushColor = 1
+def greenBrush():
+    global brushColor
+    brushColor = 2
+def blueBrush():
+    global brushColor
+    brushColor = 3
+def yellowBrush():
+    global brushColor
+    brushColor = 4
+def cyanBrush():
+    global brushColor
+    brushColor = 5
+def magentaBrush():
+    global brushColor
+    brushColor = 6
+def whiteBrush():
+    global brushColor
+    brushColor = 7
+def grayBrush():
+    global brushColor
+    brushColor = 8
 
-def click(event):
-    print(pyautogui.position())
+# def debug():
+#     print(mapArray)
 
-window.bind("<Button-1>", click)
-label = tkinter.Label(inputFrame, text = "")
-entryX = tkinter.Entry(inputFrame)
-entryY = tkinter.Entry(inputFrame)
+# def click(event): 
+#     print(pyautogui.position())
+
+def jump(height,time):
+    int
+    v = height * 4 / time
+    g = height * 8 / (time * time)
+    return f"{v},{g}"
+
+
+
+def save():
+    f = open(mapName.get()+".dat","w")
+    for y in range(mapY):
+        for x in range(mapX):
+            f.write(str(mapArray[x][y]))
+        f.write("\n") 
+    f.write("!" + position.get())
+    f.write("\n@" + size.get())
+    f.write("\n#" + jump(int(jumpHeight.get()), int(jumpTime.get())) + speed.get())
+
+
+
+
+# window.bind("<Button-1>", click)
+mapSizeAlert = tkinter.Label(inputFrame, text = "")
+XLabel = tkinter.Label(inputFrame, text = "맵의 가로 길이 입력")
+YLabel = tkinter.Label(inputFrame, text = "맵의 세로 길이 입력")
+positionLabel = tkinter.Label(inputFrame, text = "시작 좌표 입력 \nx,y의 형식으로")
+sizeLabel = tkinter.Label(inputFrame, text = "플레이어 크기 입력 \nx,y의 형식으로")
+jumpHeightLabel = tkinter.Label(inputFrame, text = "점프 높이 입력")
+jumpTimeLabel = tkinter.Label(inputFrame, text = "점프 시간 입력")
+mapNameAlert = tkinter.Label(inputFrame, text = "저장할 이름 입력")
+speedLabel = tkinter.Label(inputFrame, text = "이동 속도 입력")
+
+XEntry = tkinter.Entry(inputFrame)
+YEntry = tkinter.Entry(inputFrame)
+jumpHeight = tkinter.Entry(inputFrame)
+jumpTime = tkinter.Entry(inputFrame)
+mapName = tkinter.Entry(inputFrame)
+speed = tkinter.Entry(inputFrame)
+position = tkinter.Entry(inputFrame)
+size = tkinter.Entry(inputFrame)
+
 button = tkinter.Button(inputFrame, text = "확인", command = sizeChange)
-debugbutton = tkinter.Button(inputFrame, text = "배열", command = debug)
+saveButton = tkinter.Button(inputFrame, text = "저장", command = save)
+#debugbutton = tkinter.Button(inputFrame, text = "배열 ", command = debug)
+blackButton = tkinter.Button(inputFrame, command = blackBrush, bg = "black", width = 5)
+redButton = tkinter.Button(inputFrame, command = redBrush, bg = "red", width = 5)
+greenButton = tkinter.Button(inputFrame, command = greenBrush, bg = "green", width = 5)
+blueButton = tkinter.Button(inputFrame, command = blueBrush, bg = "blue", width = 5)
+yellowButton = tkinter.Button(inputFrame, command = yellowBrush, bg = "yellow", width = 5)
+cyanButton = tkinter.Button(inputFrame, command = cyanBrush, bg = "cyan", width = 5)
+magentaButton = tkinter.Button(inputFrame, command = magentaBrush, bg = "magenta", width = 5)
+whiteButton = tkinter.Button(inputFrame, command = whiteBrush, bg = "white", width = 5)
+grayButton = tkinter.Button(inputFrame, command = grayBrush, bg = "gray", width = 5)
+
 canvas = tkinter.Canvas()
 
-label.grid()
-entryX.grid()
-entryY.grid()
+mapSizeAlert.grid()
+XLabel.grid()
+XEntry.grid()
+YLabel.grid()
+YEntry.grid()
 button.grid()
-debugbutton.grid()
+#debugbutton.grid()
 
+blackButton.grid()
+redButton.grid()
+greenButton.grid()
+blueButton.grid()
+yellowButton.grid()
+cyanButton.grid()
+magentaButton.grid()
+whiteButton.grid()
+grayButton.grid()
+
+positionLabel.grid()
+position.grid()
+sizeLabel.grid()
+size.grid()
+jumpHeightLabel.grid()
+jumpHeight.grid()
+jumpTimeLabel.grid()
+jumpTime.grid()
+speedLabel.grid()
+speed.grid()
+mapNameAlert.grid()
+mapName.grid()
+saveButton.grid()
 window.mainloop()
