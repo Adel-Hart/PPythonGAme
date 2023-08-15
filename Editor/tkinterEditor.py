aa92616ec0a4379d0e57b2e51c2864775689f54b
arischann <liber1238@gmail.com>
2023-08-15 16:09:54 +0900

    도착지점 설정

--- Editor/tkinterEditor.py
+++ Editor/tkinterEditor.py
@@ -2,6 +2,7 @@ import tkinter
 import pyautogui
 import os
 import ctypes
+import math
 
 brushCheck = True
 brushColor = 0
@@ -10,6 +11,8 @@ mapX = 0 #맵의 가로 타일 수
 mapY = 0 #맵의 세로 타일 수
 playerX = 0 #플레이어 시작 X좌표(캔버스 기준)
 playerY = 0 #플레이어 시작 Y좌표(캔버스 기준)
+goalX = 0
+goalY = 0
 tileSize = 50 #타일의 크기
 mapOrigin = 300 # 캔버스의 시작 X좌표
 mapArray = []
@@ -18,15 +21,12 @@ user32 = ctypes.windll.user32
 SCRSIZEX = user32.GetSystemMetrics(0)-mapOrigin-50 #화면의 해상도 (픽셀수) 구하기 가로, 왼쪽 여유공간 600, 오른쪽 여유공간 50
 SCRSIZEY = user32.GetSystemMetrics(1)-50 #세로, 여유로 50 남김
 
-
-
-def sizeChange(): #2차원 배열 크기 변경, 캔버스 생성
+def drawMap(): #맵 생성
     global mapX
     global mapY
     global mapArray
     global canvas
     global tileSize
-    global characterCanvas
 
     if XEntry.get().isdigit() and YEntry.get().isdigit() and int(XEntry.get()) >= 1 and int(XEntry.get()) >= 1 and int(XEntry.get()) <= 150 and int(XEntry.get()) <= 150: #입력값이 정수인지 확인, 1 이상인지 확인
         mapSizeAlert.config(text = XEntry.get() + ", " + YEntry.get())
@@ -39,24 +39,21 @@ def sizeChange(): #2차원 배열 크기 변경, 캔버스 생성
 
         canvas = tkinter.Canvas(window, width = mapX * tileSize, height = mapY * tileSize)
         mapArray = [[0 for y in range(mapY)] for x in range(mapX)]
-       
-        drawGrid()
+
+        for x in range(mapX):
+            for y in range(mapY):
+                canvas.create_rectangle(tileSize * x, tileSize * y, tileSize * (x + 1), tileSize * (y + 1), fill = "black")
+
+        for x in range(mapX): canvas.create_line(tileSize * x, 0, tileSize * x, tileSize * (y + 1), fill = "gray") # 세로줄긋기
+        for y in range(mapY): canvas.create_line(0, tileSize * y, tileSize * (x + 1), tileSize * y, fill = "gray") # 가로줄긋기
+
+        canvas.bind("<Button-1>", colorChange) #클릭 / 드래그 감지
+        canvas.bind("<B1-Motion>", colorChange) 
+
+        canvas.place(x = mapOrigin, y = 0)
+
     else:
         mapSizeAlert.config(text = "1 이상 150 이하의 정수 입력")
-
-def drawGrid(): #격자 그리기
-    global canvas
-    for x in range(mapX):
-        for y in range(mapY):
-            canvas.create_rectangle(tileSize * x, tileSize * y, tileSize * (x + 1), tileSize * (y + 1), fill = "black")
-
-    for x in range(mapX): canvas.create_line(tileSize * x, 0, tileSize * x, tileSize * (y + 1), fill = "gray") # 세로줄긋기
-    for y in range(mapY): canvas.create_line(0, tileSize * y, tileSize * (x + 1), tileSize * y, fill = "gray") # 가로줄긋기
-
-    canvas.bind("<Button-1>", colorChange) #클릭 / 드래그 감지
-    canvas.bind("<B1-Motion>", colorChange) 
-
-    canvas.place(x = mapOrigin, y = 0)
 
 def colorChange(event): #색 변경
     global canvas
@@ -64,9 +61,11 @@ def colorChange(event): #색 변경
     x = (pyautogui.position()[0] - mapOrigin) // tileSize 
     y = (pyautogui.position()[1]) // tileSize
     if x < mapX and y < mapY:
+
         if brushCheck: #타일 색을 선택했을 경우
             mapArray[x][y] = brushColor
             canvas.create_rectangle(x * tileSize + 1, y * tileSize + 1, x * tileSize + tileSize - 1, y * tileSize + tileSize - 1, fill = colorTuple[brushColor]) # 1씩 작게 채움으로써 grid를 남긴다
+        
         else: #스위치 색을 선택했을 경우
             mapArray[x][y] = colorTuple[brushColor][0]
             canvas.create_rectangle(x * tileSize + 1, y * tileSize + 1, x * tileSize + tileSize - 1, y * tileSize + tileSize - 1, fill = "black")
@@ -99,20 +98,41 @@ def save(): #맵 파일 작성
         f.write("\n@" + playerWidth.get() + "," + playerHeight.get())
         f.write("\n#" + jump(float(jumpHeight.get()), float(jumpTime.get())) + speed.get())
         f.write("\n$" + background.get())
+        f.write("\n%" + f"{goalX},{goalY}")
         f.close
         return True
     except: # 오류 발생시 실패를 알린다
         print("저장 실패")
         return False
     
-def playerPos(event): #플레이어 시작 좌표
+def player(event): #플레이어 생성
+    global characterCanvas
     global playerX
     global playerY
-    if pyautogui.position()[0] >= mapOrigin and pyautogui.position()[0] <= tileSize * mapX + mapOrigin and pyautogui.position()[1] >= 0 and pyautogui.position()[1] <= tileSize * mapY:
+    if mapOrigin <= pyautogui.position()[0] <= tileSize * mapX + mapOrigin and 0 <= pyautogui.position()[1] <= tileSize * mapY: #클릭 좌표가 맵 안인지
         playerX = round((pyautogui.position()[0] - mapOrigin) / tileSize, 1)
         playerY = round(pyautogui.position()[1] / tileSize, 1)
         positionLabel.config(text = f"{playerX},{playerY}")
-    character()
+        if isNumeric(playerWidth.get()) and isNumeric(playerHeight.get()): #플레이어 키와 너비가 실수인지 확인
+            
+            if playerX - float(playerWidth.get()) / 2 <= 0:
+                playerX = float(playerWidth.get()) / 2 + 0.1
+
+            elif playerX + float(playerWidth.get()) / 2 >= int(XEntry.get()):
+                playerX = int(XEntry.get()) - float(playerWidth.get()) / 2 - 0.1
+            
+            if playerY - float(playerHeight.get()) / 2 <= 0:
+                playerY = float(playerHeight.get()) / 2 + 0.1
+            elif playerY + float(playerHeight.get()) / 2 >= int(YEntry.get()):
+                playerY = int(YEntry.get()) - float(playerHeight.get()) / 2 - 0.1
+            
+            mouseX = (playerX - float(playerWidth.get()) / 2) * tileSize + mapOrigin #마우스로 클릭한 지점이 맵에서 X 좌표인지(타일기준)
+            mouseY = (playerY - float(playerHeight.get()) / 2) * tileSize #Y
+        
+            characterCanvas.destroy()
+            characterCanvas = tkinter.Canvas(width = float(playerWidth.get()) * tileSize, height = float(playerHeight.get()) * tileSize)
+            characterCanvas.create_rectangle(0, 0, float(playerWidth.get()) * tileSize, float(playerHeight.get()) * tileSize, fill = "olive")
+            characterCanvas.place(x = mouseX , y = mouseY) 
 
 def isNumeric(s): #문자열 실수 판단
     try:
@@ -121,31 +141,43 @@ def isNumeric(s): #문자열 실수 판단
     except ValueError:
         return False
  
-def character(): #플레이어 캐릭터 생성
-    global characterCanvas
-    global playerX
-    global playerY
+def goal(evnet): #도착지점 생성
+    global goalCanvas
+    global goalX
+    global goalY
 
-    if isNumeric(playerWidth.get()) and isNumeric(playerHeight.get()): #플레이어 키와 너비가 실수인지 확인
-        
-        if playerX - float(playerWidth.get()) / 2 <= 0:
-            playerX = float(playerWidth.get()) / 2 + 0.1
+    if mapOrigin <= pyautogui.position()[0] <= tileSize * mapX + mapOrigin and 0 <= pyautogui.position()[1] <= tileSize * mapY: #클릭 좌표가 맵 안인지
+        goalX = round((pyautogui.position()[0] - mapOrigin) / tileSize, 2)
+        goalY = round(pyautogui.position()[1] / tileSize, 2)
 
-        elif playerX + float(playerWidth.get()) / 2 >= int(XEntry.get()):
-            playerX = int(XEntry.get()) - float(playerWidth.get()) / 2 - 0.1
-        
-        if playerY - float(playerHeight.get()) / 2 <= 0:
-            playerY = float(playerHeight.get()) / 2 + 0.1
-        elif playerY + float(playerHeight.get()) / 2 >= int(YEntry.get()):
-            playerY = int(YEntry.get()) - float(playerHeight.get()) / 2 - 0.1
-        
-        mouseX = (playerX - float(playerWidth.get()) / 2) * tileSize + mapOrigin #마우스로 클릭한 지점이 맵에서 X 좌표인지(타일기준)
-        mouseY = (playerY - float(playerHeight.get()) / 2) * tileSize #Y
-    
-        characterCanvas.destroy()
-        characterCanvas = tkinter.Canvas(width = float(playerWidth.get()) * tileSize, height = float(playerHeight.get()) * tileSize)
-        characterCanvas.create_rectangle(0, 0, float(playerWidth.get()) * tileSize, float(playerHeight.get()) * tileSize, fill = "olive")
-        characterCanvas.place(x = mouseX , y = mouseY) 
+        if goalX - 0.5 <= 0: #도착지점 X값 조정
+            goalX = 0.5
+        elif goalX + 0.5 >= int(XEntry.get()) :
+            goalX = int(XEntry.get()) - 0.5
+        else:
+            if goalX - math.trunc(goalX) <= 0.25:
+                goalX = math.trunc(goalX)
+            elif 0.25 < goalX - math.trunc(goalX) <= 0.75:
+                goalX = math.trunc(goalX) + 0.5
+            elif 0.75 < goalX - math.trunc(goalX):
+                goalX = math.trunc(goalX) + 1
+
+        if goalY - 1 <= 0: #도착지점 Y값 조정
+            goalY = 1
+        elif goalY + 1 >= int(XEntry.get()) :
+            goalY = int(XEntry.get()) - 1
+        else:
+            if goalY - math.trunc(goalY) <= 0.25: 
+                goalY = math.trunc(goalY)
+            elif 0.25 < goalY - math.trunc(goalY) <= 0.75:
+                goalY = math.trunc(goalY) + 0.5
+            elif 0.75 < goalY - math.trunc(goalY):
+                goalY = math.trunc(goalY) + 1
+
+        goalCanvas.destroy()
+        goalCanvas = tkinter.Canvas(width = tileSize, height = 2 * tileSize)
+        goalCanvas.create_rectangle(0, 0,  tileSize, 2 * tileSize, fill = "purple")
+        goalCanvas.place(x = (goalX - 0.5) * tileSize + mapOrigin , y = (goalY - 1) * tileSize) 
 
 
 # ------------------------ GUI 요소 생성 ------------------------
@@ -155,7 +187,8 @@ window.title("맵에디터") #창의 이름
 
 window.resizable(False, False) #창 크기 조절 가능 여부
 window.attributes("-fullscreen", True) #전체화면
-window.bind("<Button-3>", playerPos)
+window.bind("<Button-3>", player)
+window.bind("<Button-2>", goal)
 
 #레이블 생성
 mapSizeAlert = tkinter.Label(window, text = "")
@@ -182,7 +215,7 @@ playerHeight = tkinter.Entry(window)
 background = tkinter.Entry(window)
 
 #버튼 생성
-mapButton = tkinter.Button(window, text = "맵 생성", command = sizeChange)
+mapButton = tkinter.Button(window, text = "맵 생성", command = drawMap)
 saveButton = tkinter.Button(window, text = "맵 저장", command = save)
 
 colorButton = []
@@ -196,6 +229,7 @@ for i in range(7):
 #캔버스 생성
 canvas = tkinter.Canvas()
 characterCanvas = tkinter.Canvas()
+goalCanvas = tkinter.Canvas()
 
 
 # ------------------------ GUI 배치 ------------------------
