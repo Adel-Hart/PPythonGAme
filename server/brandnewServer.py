b03a7a7fd0270a70658d635025d30204929f5f79
silverfish3560 <silverfish3560@gmail.com>
2023-09-03 19:20:56 +0900

    준비 버튼만 추가, heartbeat 안되는 문제 수정

--- server/brandnewServer.py
+++ server/brandnewServer.py
@@ -192,6 +192,7 @@ class Handler(): #각 클라이언트의 요청을 처리함 스레드로 분리
         while self.heartStack <= self.howAlive:
             time.sleep(5) #5초간격으로, 스레드 안에서 진행이라 논 블락킹이다! ㅎㅎ
             self.sendMsg("7777") #7777이라는 hearbeat 신호 보내기 
+            self.heartStack += 1
             #응답 없으면 stack 1쌓임, 응답 7780 받으면 ok 
             # if self.msg == "7780": #recv스레드로 부터 받은 신호가 7780이면
             #     heartStack = 0 #잘 살아 있으니, 스택 초기화 해주자!
